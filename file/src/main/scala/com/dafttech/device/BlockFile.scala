c3e0487851bf25c3acf56f1d29ba50794cffff0e
Pierre Kisters <pierrekisters@gmail.com>
2017-04-09 18:50:31 +0200

    added blockFile

--- file/src/main/scala/com/dafttech/device/BlockFile.scala
+++ file/src/main/scala/com/dafttech/device/BlockFile.scala
@@ -4,6 +4,7 @@ import java.io.RandomAccessFile
 import java.nio.file.{Files, Path}
 
 import com.dafttech.rorqual.BlockStorage
+import monix.eval.Task
 import monix.reactive.Observable
 import scodec.bits.ByteVector
 
@@ -13,15 +14,23 @@ import scodec.bits.ByteVector
 class BlockFile(path: Path) extends BlockStorage {
   override def size: Long = Files.size(path)
 
-  private val randomAccessFile = new RandomAccessFile(path.toFile, "rw")
+  private val randomAccessFile = new RandomAccessFile(path.toFile, "rws")
 
   override def read(index: Long, length: Long, chunkSize: Long): Observable[ByteVector] =
-    Observable.fromIterable(0 until (size / chunkSize)).map { blockOffset =>
-      randomAccessFile.readFully(null, index.toInt, length.toInt)
-      ???
+    Observable.fromIterable(index until (index + length)).bufferTumbling(chunkSize.toInt).map { chunk =>
+      val chunkIndex = chunk.head
+      val chunkLength = chunk.last - chunkIndex
+      val chunkArray = new Array[Byte](chunkLength.toInt)
+      randomAccessFile.readFully(chunkArray, chunkIndex.toInt, chunkLength.toInt)
+      ByteVector(chunkArray)
     }
 
-  override def write(index: Long, data: Observable[ByteVector]): Unit = ???
+  override def write(index: Long, data: Observable[ByteVector]): Task[Unit] =
+    data.foldLeftL(index) { (offset, block) =>
+      randomAccessFile.write(block.toArray, offset.toInt, block.size.toInt)
 
-  override def close(): Unit = ???
+      offset + block.size
+    }.map(_ => ())
+
+  override def close(): Unit = randomAccessFile.close()
 }
