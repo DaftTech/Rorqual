1b7fba6e9d669976614cb4348c87325b12b9e893
Pierre Kisters <pierrekisters@gmail.com>
2017-04-24 11:57:05 +0200

    observable.parse test success

---
+++ core/src/test/scala/com.dafttech.rorqual/ParsableObservableTest.scala
@@ -0,0 +1,32 @@
+package com.dafttech.rorqual
+
+import monix.execution.Scheduler.Implicits.global
+import monix.reactive.Observable
+import scodec.bits.ByteVector
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+/**
+  * Created by pierr on 24.04.2017.
+  */
+object ParsableObservableTest {
+  def main(args: Array[String]): Unit = {
+    object SmallDevice extends BlockStorageDevice {
+      override val id: String = "asdf"
+
+      override def size: Long = ???
+
+      override def blockSize = 16
+
+      override def open(writable: Boolean): BlockStorageHandle = ???
+    }
+
+    def bvec(size: Int) = ByteVector((0 until size).map(_.toByte))
+
+    val observable: Observable[ByteVector] = Observable(bvec(1), bvec(4), bvec(16))
+
+    val result = SmallDevice.align(0, observable).map(_.size)
+    println(Await.result(result.toListL.runAsync, Duration.Inf))
+  }
+}
